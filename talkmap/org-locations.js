2faa09c46419742892bb301aa6238eae567a9395
Ming <42477855+ming1993li@users.noreply.github.com>
2021-03-31 02:27:26 -0400

    Add files via upload

---
+++ talkmap/org-locations.js
@@ -0,0 +1,72 @@
+var addressPoints = [
+  [
+    "Shanghai, China",
+    31.2252985,
+    121.4890497
+  ],
+  [
+    "Shangqiu, China",
+    34.417097,
+    115.6508622
+  ],
+  [
+    "Nanjing, China",
+    32.0609736,
+    118.7916458
+  ],
+  [
+    "Worcester, Massachusetts, US",
+    42.2625932,
+    -71.8022934
+  ],
+  [
+    "New York City, US",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Chengdu, China",
+    30.6624205,
+    104.0633219
+  ],
+  [
+    "Charlotte, North Carolina, US",
+    35.2272,
+    -80.843083
+  ],
+  [
+    "Xian, China",
+    34.3468421,
+    108.9365887
+  ],
+  [
+    "Beijing, China",
+    39.906217,
+    116.3912757
+  ],
+  [
+    "Shijiazhuang, China",
+    38.04,
+    114.47
+  ],
+  [
+    "Chapel Hill, North Carolina, US",
+    35.9131542,
+    -79.05578
+  ],
+  [
+    "Lanzhou, China",
+    36.0606904,
+    103.8305691
+  ],
+  [
+    "Washington, D.C., US",
+    38.8949855,
+    -77.0365708
+  ],
+  [
+    "Chongqing, China",
+    29.5585712,
+    106.5492822
+  ]
+];
